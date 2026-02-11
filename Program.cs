aba2e88b2c77712a1b9314ab85fac9f570861ac9
hemersonAlves <hemerson.o@aluno.senai.br>
2026-02-11 19:37:35 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Variáveis e Operações Básicas
+#region 1. Calculadora de Média
+
+Console.Write("Digite sua primeira nota com 2 casas decimais: ");
+decimal nota01 = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite sua segunda nota com 2 casas decimais: ");
+decimal nota02 = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite sua terceira nota com 2 casas decimais: ");
+decimal nota03 = Convert.ToDecimal(Console.ReadLine());
+
+decimal media = (nota01 + nota02 + nota03) / 3;
+
+Console.WriteLine(media);
+
+#endregion
